54cc3c5b538a719819dd1dde0e14a6037e2e505e
Sean <crimsonsand@live.com>
2022-04-09 18:23:47 -0700

    Add Submit button for survey

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,12 @@
-const ul_1 = document.querySelector(".option1");
-const ul_2 = document.querySelector(".option2");
-const ul_3 = document.querySelector(".option3");
-const ul_4 = document.querySelector(".option4");
-const ul_5 = document.querySelector(".option5");
+const firstQuestion = $("#firstQuestion").val();
+const secondQuestion = $("#secondQuestion").val();
+const thirdQuestion = $("#thirdQuestion").val();
+const fourthQuestion = $("#fourthQuestion").val();
+const fifthQuestion = $("#fifthQuestion").val();
 
-const firstQuestion = document.querySelector(".firstQuestion");
-const secondQuestion = document.querySelector(".secondQuestion");
-const thirdQuestion = document.querySelector(".thirdQuestion");
-const fourthQuestion = document.querySelector(".fourthQuestion");
-const fifthQuestion = document.querySelector(".fifthQuestion");
-
-const survey = document.querySelector(".survey");
-const end = document.querySelector(".end");
-
-// First question
-ul_1.addEventListener("click", function() {
-  firstQuestion.getElementsByClassName.display = "none";
-  secondQuestion.getElementsByClassName.display = "block";
-});
-
-// Second question
-ul_2.addEventListener("click", function() {
-  secondQuestion.getElementsByClassName.display = "none";
-  thirdQuestion.getElementsByClassName.display = "block";
-});
-
-// Third question
-ul_3.addEventListener("click", function() {
-  thirdQuestion.getElementsByClassName.display = "none";
-  fourthQuestion.getElementsByClassName.display = "block";
-});
-
-// Fourth question
-ul_4.addEventListener("click", function() {
-  fourthQuestion.getElementsByClassName.display = "none";
-  fifthQuestion.getElementsByClassName.display = "block";
-});
-
-// Display Answer
-ul_5.addEventListener("click", function() {
-  fifthQuestion.getElementsByClassName.display = "none";
-  survey.getElementsByClassName.display = "none";
-  end.getElementsByClassName.display = "block";
+$(document).ready(function() {
+  $("#name").submit(function(event) {
+    $("#firstQestion").show();
+    event.preventDefault();
+  });
 });
