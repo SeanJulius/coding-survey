b6c67b71571db50f416bbed54856cdc55f2e6c00
Sean <crimsonsand@live.com>
2022-04-09 23:22:32 -0700

    Add README file

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,29 @@ $(document).ready(function() {
     event.preventDefault();
 
     $("#firstQuestion").show();
+    $("#secondQuestion").show();
+    $("#thirdQuestion").show();
+    $("#fourthQuestion").show();
+    $("#fifthQuestion").show();
   });
 });
 
 $(document).ready(function() {
-  $("#formTwo").submit(function(event) {
+  $("form#firstQuestion").submit(function(event) {
+    const firstQuestion = parseInt($("select#firstQuestion").val());
+    const secondQuestion = parseInt($("select#secondQuestion").val());
+    const thirdQuestion = parseInt($("select#thirdQuestion").val());
+    const fourthQuestion = parseInt($("select#fourthQuestion").val());
+    const fifthQuestion = parseInt($("select#fifthQuestion").val());
+
+    if (firstQuestion + secondQuestion + thirdQuestion + fourthQuestion + fifthQuestion <= 7) {
+      alert("C# is the coding path for you");
+    } else if (firstQuestion + secondQuestion + thirdQuestion + fourthQuestion + fifthQuestion === 8, 9, 10, 11) {
+      alert("Rust is the coding path for you");
+    } else { (firstQuestion + secondQuestion + thirdQuestion + fourthQuestion + fifthQuestion >=12) 
+      alert("Java Script is the coding path for you");
+    }
+
     event.preventDefault();
-    
-    $("#secondQuestion").show();
   });
 });
