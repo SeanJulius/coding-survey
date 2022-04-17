46aa63a6de5128eb02fe25911e58aff0dd019965
Sean <crimsonsand@live.com>
2022-04-16 22:33:31 -0700

    Add a different route to submitting form, no alerts and added some color

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,22 @@
-const firstQuestion = $("#firstQuestion").val();
-const secondQuestion = $("#secondQuestion").val();
-const thirdQuestion = $("#thirdQuestion").val();
-const fourthQuestion = $("#fourthQuestion").val();
-const fifthQuestion = $("#fifthQuestion").val();
+$(document).ready(function(){
+  $("#formOne").submit(function(event){
+event.preventDefault();
 
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    event.preventDefault();
+      const questionOneInput=parseInt($("#firstQuestion").val());
+      const questionTwoInput=parseInt($("#secondQuestion").val());
+      const questionThreeInput=parseInt($("#thirdQuestion").val());
+      const questionFourInput=parseInt($("#fourthQuestion").val());
+      const questionFiveInput=parseInt($("#fifthQuestion").val());
 
-    $("#firstQuestion").show();
-    $("#secondQuestion").show();
-    $("#thirdQuestion").show();
-    $("#fourthQuestion").show();
-    $("#fifthQuestion").show();
-  });
-});
-
-$(document).ready(function() {
-  $("form#firstQuestion").submit(function(event) {
-    const firstQuestion = parseInt($("select#firstQuestion").val());
-    const secondQuestion = parseInt($("select#secondQuestion").val());
-    const thirdQuestion = parseInt($("select#thirdQuestion").val());
-    const fourthQuestion = parseInt($("select#fourthQuestion").val());
-    const fifthQuestion = parseInt($("select#fifthQuestion").val());
-
-    if (firstQuestion + secondQuestion + thirdQuestion + fourthQuestion + fifthQuestion <= 7) {
-      alert("C# is the coding path for you");
-    } else if (firstQuestion + secondQuestion + thirdQuestion + fourthQuestion + fifthQuestion === 8, 9, 10, 11) {
-      alert("Rust is the coding path for you");
-    } else { (firstQuestion + secondQuestion + thirdQuestion + fourthQuestion + fifthQuestion >=12) 
-      alert("Java Script is the coding path for you");
-    }
-
-    event.preventDefault();
-  });
-});
+      const final = questionOneInput+questionTwoInput+questionThreeInput+questionFourInput+questionFiveInput
+        if (final <5){
+          $("#nullResult").show();
+        } else if (final <9){
+          $("#firstResult").show();
+        } else if  (final <13 ){
+          $("#secondResult").show();
+        } else 
+          $("#thirdResult").show();
+          $("#formOne")[0].reset();   
+      });
+    });
